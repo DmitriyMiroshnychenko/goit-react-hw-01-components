0a52f9623d752323f798df94c179755e22b0d91a
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2023-05-20 12:17:17 +0300

    removed old markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { SocialProfile } from './SocialProfile/SocialProfile';
+import user from './SocialProfile/data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from './FriendList/data/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <SocialProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
